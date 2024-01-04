98026134e15219cfc1007df37a8271c45d41c0a1
Sunil Chouhan <sunil.chouhan@cubastion.com>
2024-01-04 16:21:15 +0530

    pages components are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateNewJobComponent } from './pages/create-new-job/create-new-job.component';
+import { HomeComponent } from './pages/home/home.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
+import { JobListingComponent } from './pages/job-listing/job-listing.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateNewJobComponent,
+    HomeComponent,
+    JobDetailsComponent,
+    JobListingComponent,
+    JobsComponent,
+    MyJobsComponent,
+    NotFoundComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
