c9a4c5a271580e197ed1cf35e6f87782d73e0c61
Sunil Chouhan <sunil.chouhan@cubastion.com>
2024-01-04 18:08:57 +0530

    nav bar completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
+import { CreateNewJobComponent } from './pages/create-new-job/create-new-job.component';
+import { JobListingComponent } from './pages/job-listing/job-listing.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+ {path:'home', component : HomeComponent},
+ {path:'login', component : LoginComponent},
+ {path:'registration',component: RegistrationComponent},
+ {path:'jobs', component: JobsComponent},
+ {path: 'job-details', component:JobDetailsComponent},
+ {path: 'new-job', component:CreateNewJobComponent},
+ {path:'job-listing', component:JobListingComponent},
+ {path:'my-jobs', component:MyJobsComponent},
+ {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
